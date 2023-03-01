45f06ab00cd16987cbb3ccea6377263a019088e1
염정아 <dua9920@daum.net>
2023-03-01 11:29:49 +0900

    [FE] hotfix : project delete

--- FE/s08p21e101/src/core/tmp/userAPI.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import http from "@/common/axios";
-
-export const logIn = createAsyncThunk("users/logIn", async (id: number) => {
-  const { data } = await http.get(`/users/${id}`);
-  console.log(data);
-  return data;
-});
--- FE/s08p21e101/src/pages/mypage.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AppLayout from "@/components/layout/AppLayout";
-
-export default function mypage() {
-  return <AppLayout>mypage</AppLayout>;
-}
