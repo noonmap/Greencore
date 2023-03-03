9e74bd0d730492ea3579eb7b4c9d492e8c7dc62d
yeomss <dua9920@daum.net>
2023-03-03 16:39:47 +0900

    [FE] feat : front project init (next.js, redux-toolkit, typescript, react.js, tailwindCSS, Pretendard Font, swr)

---
+++ FE/s08p21e101/lib/http.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const serverUrl = process.env.NODE_ENV == "production" ? process.env.APP_SERVER_URL : "http://localhost:8080";
+
+export default axios.create({
+  baseURL: serverUrl + "/api",
+
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  },
+
+  withCredentials: true,
+});
