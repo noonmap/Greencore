7823106e06f066b77ee3fd1ee141bbd969cca3ce
rlthdqls <rlthdqls@gmail.com>
2023-03-09 22:14:14 +0900

    [BE] feat : 일정 목업

--- BE/chicochico/src/main/java/com/chicochico/domain/schedule/repository/ScheduleRepository.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/schedule/repository/ScheduleRepository.java
@@ -1,5 +1,9 @@
 package com.chicochico.domain.schedule.repository;
 
 
-public interface ScheduleRepository {
+import com.chicochico.domain.schedule.entity.ScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
 }
