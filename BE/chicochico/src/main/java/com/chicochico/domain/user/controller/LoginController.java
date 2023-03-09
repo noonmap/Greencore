97eb663ef720d71d63daad2562ad76e4ec121ac0
sha990403 <sha990403@gmail.com>
2023-03-09 12:02:43 +0900

    [BE] feat : 유저(인증관련) Controller 목업 추가 S08P22E101-97

---
+++ BE/chicochico/src/main/java/com/chicochico/domain/user/controller/LoginController.java
@@ -0,0 +1,58 @@
+package com.chicochico.domain.user.controller;
+
+
+import com.chicochico.common.dto.ResultDto;
+import com.chicochico.domain.user.dto.LoginRequestDto;
+import com.chicochico.domain.user.service.LoginService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = "유저 로그인 API")
+public class LoginController {
+
+	private final LoginService loginService;
+
+
+	@PostMapping("/login")
+	@ApiOperation(value = "로그인합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> login(@RequestBody LoginRequestDto loginRequestDto) {
+		loginService.login(loginRequestDto);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		// httpHeaders.add(); 헤더에 토큰 추가
+
+		return ResponseEntity.ok().headers(httpHeaders).body(ResultDto.ofSuccess());
+	}
+
+
+	@PostMapping("/login/github")
+	@ApiOperation(value = "깃허브로 로그인합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> githubLogin(@RequestBody Map<String, Object> loginRequestHeader) {
+		loginService.githubLogin(loginRequestHeader);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		// httpHeaders.add(); 헤더에 토큰 추가
+
+		return ResponseEntity.ok().headers(httpHeaders).body(ResultDto.ofSuccess());
+	}
+
+
+	@PostMapping("/refresh")
+	@ApiOperation(value = "엑세스 토큰을 발급합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> createAccessToken(@RequestHeader Map<String, Object> loginRequestHeader) {
+		loginService.createAccessToken(loginRequestHeader);
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
+}
--- BE/chicochico/src/main/java/com/chicochico/domain/user/controller/UserController.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/user/controller/UserController.java
@@ -1,5 +1,68 @@
 package com.chicochico.domain.user.controller;
 
 
+import com.chicochico.common.dto.ResultDto;
+import com.chicochico.domain.user.dto.RegisterRequestDto;
+import com.chicochico.domain.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = "유저 API")
 public class UserController {
+
+	private final UserService userService;
+
+
+	@PostMapping("/user")
+	@ApiOperation(value = "회원가입을 합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> signUp(@RequestBody RegisterRequestDto registerRequestDto) {
+		userService.createUser(registerRequestDto);
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
+
+	@GetMapping("/user/{nickname}")
+	@ApiOperation(value = "닉네임 중복확인을 합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> checkNickname(@PathVariable("nickname") String nickname) {
+		userService.checkNickname(nickname);
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
+
+	@PostMapping("/user/password")
+	@ApiOperation(value = "현재 비밀번호를 확인합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> checkPassword(@RequestBody Map<String, Object> userRequestDto) {
+		userService.checkPassword(userRequestDto);
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
+
+	@PostMapping("/user/password")
+	@ApiOperation(value = "비밀번호를 수정합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> modifyPassword(@RequestBody Map<String, Object> userRequestDto) {
+		userService.modifyPassword(userRequestDto);
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
+
+	@PostMapping("/user")
+	@ApiOperation(value = "회원탈퇴를 합니다.", notes = "")
+	public ResponseEntity<ResultDto<Boolean>> deleteUser() {
+		userService.deleteUser();
+
+		return ResponseEntity.ok().body(ResultDto.ofSuccess());
+	}
+
 }
