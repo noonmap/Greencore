200fd2fcf8a1936e6fb177aedf9a4f8cdfc0e914
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-03-09 11:55:21 +0900

    [BE] feat : 유저(인증관련) dto 목업 추가 S08P22E101-97

--- BE/chicochico/src/main/java/com/chicochico/domain/user/dto/AuthCodeRequestDto.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/user/dto/AuthCodeRequestDto.java
@@ -1,8 +1,16 @@
 package com.chicochico.domain.user.dto;
 
 
+import com.chicochico.domain.user.entity.UserEntity;
+
+
 /**
  * 이메일 인증 코드 확인
  */
 public class AuthCodeRequestDto {
+
+	public UserEntity toEntity() {
+		return (UserEntity) new Object();
+	}
+
 }
--- BE/chicochico/src/main/java/com/chicochico/domain/user/dto/LoginRequestDto.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/user/dto/LoginRequestDto.java
@@ -1,8 +1,16 @@
 package com.chicochico.domain.user.dto;
 
 
+import com.chicochico.domain.user.entity.UserEntity;
+
+
 /**
  * 로그인 요청
  */
 public class LoginRequestDto {
+
+	public UserEntity toEntity() {
+		return (UserEntity) new Object();
+	}
+
 }
