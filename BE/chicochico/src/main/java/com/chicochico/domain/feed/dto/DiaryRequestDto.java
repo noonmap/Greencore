210dddfc183ccecdb985abca6ec63dd038af93a2
rlthdqls <rlthdqls@gmail.com>
2023-03-10 10:23:31 +0900

    [BE] feat : 일지 목업

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/dto/DiaryRequestDto.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/dto/DiaryRequestDto.java
@@ -1,8 +1,16 @@
 package com.chicochico.domain.feed.dto;
 
 
+import com.chicochico.domain.feed.entity.DiaryEntity;
+
+
 /**
  * 일지 생성, 수정 요청
  */
 public class DiaryRequestDto {
+
+	public DiaryEntity toEntity() {
+		return (DiaryEntity) new Object();
+	}
+
 }
