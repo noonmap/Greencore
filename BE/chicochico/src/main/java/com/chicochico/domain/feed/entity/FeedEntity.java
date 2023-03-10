2de3461cd9a8cc187d7a58e73259631da359db86
JCW <codnjs8221@naver.com>
2023-03-10 21:30:33 +0900

    [BE] feat : feed Entity 추가
    S08P22E101-263

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/entity/FeedEntity.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/entity/FeedEntity.java
@@ -1,5 +1,48 @@
 package com.chicochico.domain.feed.entity;
 
 
-public class FeedEntity {
+import com.chicochico.common.code.FeedType;
+import com.chicochico.common.code.IsDeletedType;
+import com.chicochico.common.entity.CommonEntity;
+import com.chicochico.domain.user.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name="feed_code")
+@Table(name = "feed")
+public class FeedEntity extends CommonEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity user;
+
+	@Column(nullable = false)
+	private String content;
+
+	private String imagePath;
+
+	@Column(nullable = false)
+	private Integer likeCount;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private FeedType feedCode;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private IsDeletedType isDeletedType;
 }
