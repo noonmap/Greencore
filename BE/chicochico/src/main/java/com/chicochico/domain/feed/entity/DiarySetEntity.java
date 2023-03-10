3ea3d849a259aa4a76e2aa2bc00fcf2340925fda
JCW <codnjs8221@naver.com>
2023-03-10 16:45:43 +0900

    [BE] feat : 관찰 일지 Entity 추가
    S08P22E101-262

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/entity/DiarySetEntity.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/entity/DiarySetEntity.java
@@ -1,9 +1,52 @@
 package com.chicochico.domain.feed.entity;
 
 
-import javax.persistence.Entity;
+import com.chicochico.common.code.IsDeletedType;
+import com.chicochico.common.entity.CommonEntity;
+import com.chicochico.domain.user.entity.UserEntity;
+import com.chicochico.domain.user.entity.UserPlantEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
-public class DiarySetEntity {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "diary_set") // snake_case로 설정
+public class DiarySetEntity extends CommonEntity {
+	// createdAt, updatedAt은 CommonEntity에 들어있으므로 포함하지 않아도 됨.
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id") // FK 이름 지정
+	private UserEntity user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_plant_id") // FK 이름 지정
+	private UserPlantEntity userPlant;
+
+	private String imagePath;
+
+	private Integer diaryCount;
+
+	private String title;
+
+	@Enumerated(EnumType.STRING)
+	private IsDeletedType isDeleted;
+
+	// OneToMany는 관찰일기처럼 관찰일기에서 일기 목록을 조회하는게 효율적인 경우 추가한다. (optional)
+	@OneToMany(mappedBy = "diarySet") // 연관 관계 엔티티의 매핑되는 필드 이름을 적어줌
+	private List<DiaryEntity> diaryList = new ArrayList<>();
+
 }
