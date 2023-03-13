9d58721111e973f2210d6d5aca10cda4dc4cf98b
JCW <codnjs8221@naver.com>
2023-03-13 15:07:49 +0900

    [BE] feat : JWT 인증 관련 코드 추가
    S08P22E101-274 #done

--- BE/chicochico/src/main/java/com/chicochico/config/CorsConfig.java
+++ BE/chicochico/src/main/java/com/chicochico/config/CorsConfig.java
@@ -12,8 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8080", "https://i8e201.p.ssafy.io/",
-				"http://localhost:3000")
+			.allowedOrigins("http://localhost:8080", "https://i8e201.p.ssafy.io/", "http://localhost:3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowCredentials(true);
 	}
--- BE/chicochico/src/main/java/com/chicochico/config/SpringSecurity.java
+++ BE/chicochico/src/main/java/com/chicochico/config/SpringSecurity.java
@@ -1,30 +1,68 @@
 package com.chicochico.config;
 
 
+import com.chicochico.common.service.AuthTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SpringSecurity extends WebSecurityConfigurerAdapter {
-	// Bean 써서 대체할 방법 찾아보기
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SpringSecurity {
 
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors().disable().headers().frameOptions().disable();
-	}
+	private final AuthTokenProvider tokenProvider;
 
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+
+	@Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// CSRF 설정 Disable
+		http.csrf().disable()
+			.cors().disable()
+
+			// exception handling 할 때 우리가 만든 클래스를 추가
+			.exceptionHandling()
+
+			.and()
+			.headers()
+			.frameOptions()
+			.sameOrigin()
+
+			// 시큐리티는 기본적으로 세션을 사용
+			// 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+			// 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+			.and()
+			.authorizeRequests()
+			.antMatchers("/login/**", "/logout", "/user").permitAll()
+			.anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
+
+			// JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+			.and()
+			.apply(new JwtSecurityConfig(tokenProvider));
+
+		return http.build();
 	}
 
 }
