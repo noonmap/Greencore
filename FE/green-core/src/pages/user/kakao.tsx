7066037827aa2546f03b2909e6ed2776d4445aff
yeomss <dua9920@daum.net>
2023-03-14 02:11:29 +0900

    [FE] feat : 카카오 로그인, 로그아웃 목업 구현

--- FE/green-core/src/pages/user/kakao.tsx
+++ FE/green-core/src/pages/user/kakao.tsx
@@ -1,44 +1,97 @@
 import AppLayout from '@/layout/AppLayout';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 import kakaoConfig from '~/config/kakaoConfig.json';
 
 export default function kakao() {
-  const router = useRouter();
-  const { code } = router.query;
+	const router = useRouter();
+	const { code } = router.query;
+	const [accessToken, setAccessToken] = useState('');
 
-  async function handleKakaoLogin() {
-    const restApiKey = kakaoConfig.apiKey;
-    const redirectUri = kakaoConfig.redirectUri;
+	async function handleKakaoLogin() {
+		let access_token = null;
 
-    const payload = {
-      grant_type: 'authorization_code',
-      client_id: restApiKey,
-      redirect_uri: redirectUri,
-      code,
-    };
+		try {
+			const client_id = kakaoConfig.apiKey;
+			const redirect_uri = kakaoConfig.redirectUri;
+			const client_secret = kakaoConfig.clientSecret;
 
-    console.log(payload);
-    const headers = {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    };
-    // const tokenUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&cliend_id=${restApiKey}&redirect_uri=${redirectUri}&code=${code}`;
-    const tokenUrl = `https://kauth.kakao.com/oauth/token`;
+			const payload = {
+				grant_type: 'authorization_code',
+				client_id,
+				client_secret,
+				redirect_uri,
+				code
+			};
 
-    const res = await axios.post(tokenUrl, payload);
-    console.log(res);
-  }
+			const headers = {
+				headers: {
+					'content-type': 'application/x-www-form-urlencoded'
+				}
+			};
 
-  return (
-    <AppLayout>
-      <button className='bg-blue-500 rounded' onClick={handleKakaoLogin}>
-        카카오 로그인
-      </button>
-      <div>{code}</div>
-    </AppLayout>
-  );
+			const url = `https://kauth.kakao.com/oauth/token`;
+
+			const { data } = await axios.post(url, payload, headers);
+			access_token = data.access_token;
+			setAccessToken(access_token);
+			console.log(accessToken);
+		} catch (error) {
+			console.error(error);
+		}
+
+		try {
+			const headers = {
+				headers: {
+					Authorization: `Bearer ${access_token}`
+				}
+			};
+
+			const { data } = await axios.get('https://kapi.kakao.com/v2/user/me', headers);
+			console.log(data);
+
+			const kakaoEmail = data.kakao_account.email;
+			const kakaoNickname = data.kakao_account.profile.nickname;
+			const kakaoProfileImageUrl = data.kakao_account.profile.profile_image_url;
+			const kakaoUID = data.id;
+
+			console.log(kakaoEmail, kakaoUID, kakaoNickname, kakaoProfileImageUrl);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async function handleLogOut() {
+		try {
+			const headers = {
+				headers: {
+					Authorization: `Bearer ${accessToken}`
+				}
+			};
+
+			const payload = {
+				target_id: '1696881652'
+			};
+
+			const res = await axios.post('https://kapi.kakao.com/v1/user/logout', payload, headers);
+			console.log(res);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<AppLayout>
+			<button className="bg-blue-500 rounded" onClick={handleKakaoLogin}>
+				카카오 로그인
+			</button>
+			<div>{code}</div>
+
+			<button className="bg-blue-500 rounded" onClick={handleLogOut}>
+				로그아웃
+			</button>
+		</AppLayout>
+	);
 }
