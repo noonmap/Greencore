0e31b47781ea3e4c5927554e11df5af6426f7b48
yeomss <dua9920@daum.net>
2023-03-14 09:21:11 +0900

    [FE] feat : 카카오 로그인 목업 완료

--- FE/green-core/src/layout/AppHeader.tsx
+++ FE/green-core/src/layout/AppHeader.tsx
@@ -4,21 +4,22 @@ import styles from './AppHeader.module.scss';
 
 export default function AppHeader() {
   return (
-		<div className={`flex space-x-4 items-center ${styles.headerWrap}`}>
-			<Link href="/">Home</Link>
-			<Link href="/temp/post">Post</Link>
-			<Link href="/temp">Temp</Link>
+    <div className={`flex space-x-4 items-center ${styles.headerWrap}`}>
+      <Link href='/'>Home</Link>
+      <Link href='/temp/post'>Post</Link>
+      <Link href='/temp'>Temp</Link>
 
-			{/* 정아 */}
-			<Link href="/user/signup">회원가입</Link>
-			<Link href="/user/login">로그인</Link>
-			<Link href="/user/password">새로운 비밀번호</Link>
+      {/* 정아 */}
+      <Link href='/user/signup'>회원가입</Link>
+      <Link href='/user/login'>로그인</Link>
+      <Link href='/user/password'>새로운 비밀번호</Link>
+      <Link href='/myfeed'>내 피드</Link>
 
-			{/* 승태 */}
-			<Link href="/alert">알림</Link>
+      {/* 승태 */}
+      <Link href='/alert'>알림</Link>
 
-			{/* 형규 */}
-			<Link href="/diary">일지</Link>
-		</div>
-	);
+      {/* 형규 */}
+      <Link href='/diary'>일지</Link>
+    </div>
+  );
 }
