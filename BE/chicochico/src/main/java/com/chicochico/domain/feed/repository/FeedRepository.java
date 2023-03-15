c6da00306f238c07987d2763b522536729271948
JCW <codnjs8221@naver.com>
2023-03-15 12:08:43 +0900

    [BE] fix : 팔로우 한 사람의 피드 조회 sql문 수정
    S08P22E101-280

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/repository/FeedRepository.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/repository/FeedRepository.java
@@ -2,6 +2,7 @@ package com.chicochico.domain.feed.repository;
 
 
 import com.chicochico.domain.feed.entity.FeedEntity;
+import com.chicochico.domain.user.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface FeedRepository extends JpaRepository<FeedEntity, Long> {
 
 	@Query("select f from FeedEntity f where f.user in (:userIds)")
-	Page<FeedEntity> findByUserIn(Long[] userIds, Pageable pageable);
+	Page<FeedEntity> findByUserIn(UserEntity[] userIds, Pageable pageable);
 
 }
