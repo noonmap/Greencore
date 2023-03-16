0b41f78a3ece973461976b870592085f8c022694
JCW <codnjs8221@naver.com>
2023-03-16 18:01:20 +0900

    [BE] test : 단위테스트 파일 추가
    S08P22E101-279

--- BE/chicochico/src/test/java/com/chicochico/feed/PostServiceTest.java
+++ BE/chicochico/src/test/java/com/chicochico/feed/PostServiceTest.java
@@ -1,5 +1,40 @@
 package com.chicochico.feed;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
+
+	@Nested
+	@DisplayName("")
+	class 조회 {
+
+		@Test
+		@DisplayName("유저가 존재하지 않는 경우")
+		public void test1() {
+
+		}
+
+
+		@Test
+		@DisplayName("유저가 탈퇴한 경우")
+		public void test2() {
+
+		}
+
+
+		@Test
+		@DisplayName("")
+		public void test3() {
+
+		}
+
+	}
+
 }
