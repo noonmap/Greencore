12582283ead6bce81f95a4977dc65d9767a889d4
rlthdqls <rlthdqls@gmail.com>
2023-03-20 15:33:47 +0900

    [BE] feat : 댓글 Service & Repository

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/repository/CommentRepository.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/repository/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.chicochico.domain.feed.repository;
 
 
+import com.chicochico.common.code.IsDeletedType;
 import com.chicochico.domain.feed.entity.CommentEntity;
 import com.chicochico.domain.feed.entity.FeedEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
 	List<CommentEntity> findByFeed(FeedEntity feed);
+	Page<CommentEntity> findByFeedAndIsDeleted(FeedEntity feed, Pageable pageable, IsDeletedType isDeletedType);
 	Integer countByFeed(FeedEntity feed);
 
 }
--- BE/chicochico/src/main/java/com/chicochico/domain/feed/service/CommentService.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/service/CommentService.java
@@ -1,13 +1,22 @@
 package com.chicochico.domain.feed.service;
 
 
+import com.chicochico.common.code.IsDeletedType;
+import com.chicochico.common.service.AuthService;
 import com.chicochico.domain.feed.dto.request.CommentRequestDto;
 import com.chicochico.domain.feed.entity.CommentEntity;
+import com.chicochico.domain.feed.entity.FeedEntity;
 import com.chicochico.domain.feed.repository.CommentRepository;
+import com.chicochico.domain.feed.repository.FeedRepository;
+import com.chicochico.domain.user.entity.UserEntity;
+import com.chicochico.domain.user.repository.UserRepository;
+import com.chicochico.exception.CustomException;
+import com.chicochico.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -15,6 +24,10 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
 	private final CommentRepository commentRepository;
+	private final FeedRepository feedRepository;
+	private final UserRepository userRepository;
+
+	private AuthService authService;
 
 
 	/**
@@ -25,7 +38,11 @@ public class CommentService {
 	 * @return
 	 */
 	public Page<CommentEntity> getCommentList(Long feedId, Pageable pageable) {
-		return Page.empty();
+		// 피드 찾기
+		FeedEntity feed = feedRepository.findById(feedId).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+		// comment page 형식으로 가지고 오기
+		Page<CommentEntity> commentEntityPage = commentRepository.findByFeedAndIsDeleted(feed, pageable, IsDeletedType.N);
+		return commentEntityPage;
 	}
 
 
@@ -36,6 +53,20 @@ public class CommentService {
 	 * @param commentRequestDto
 	 */
 	public void createComment(Long feedId, CommentRequestDto commentRequestDto) {
+		//피드 찾기
+		FeedEntity feed = feedRepository.findById(feedId).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+		//현재 사용자
+		Long userId = authService.getUserId();
+		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.ACCESS_TOKEN_NOT_FOUND));
+		//멘션 사용자 찾기
+		if (commentRequestDto.getMentionNickname() != null && !commentRequestDto.getMentionNickname().equals("")) {
+			String nickname = commentRequestDto.getMentionNickname();
+			Long mentionId = userRepository.findByNickname(nickname).get().getId();
+			//댓글 등록
+			commentRepository.save(commentRequestDto.toEntity(feed, user, mentionId));
+		}
+		//멘션 없는 경우 댓글 등록
+		//		commentRepository.save(commentRequestDto.toEntity(feed, user));
 
 	}
 
@@ -47,6 +78,43 @@ public class CommentService {
 	 * @param commentRequestDto
 	 */
 	public void modifyComment(Long commentId, CommentRequestDto commentRequestDto) {
+		//피드 찾기
+		CommentEntity comment = commentRepository.findById(commentId).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+		//현재 사용자
+		Long userId = authService.getUserId();
+		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.ACCESS_TOKEN_NOT_FOUND));
+
+		if (user.equals(comment.getUser())) {
+
+			//멘션 사용자 찾기
+			if (commentRequestDto.getMentionNickname() != null && !commentRequestDto.getMentionNickname().equals("")) {
+				String nickname = commentRequestDto.getMentionNickname();
+				Long mentionId = userRepository.findByNickname(nickname).get().getId();
+				//댓글 등록
+				CommentEntity newComment = CommentEntity.builder()
+					.id(commentId)
+					.content(comment.getContent())
+					.feed(comment.getFeed())
+					.mentionUserId(mentionId)
+					.isDeleted(IsDeletedType.N)
+					.user(user)
+					.build();
+
+				commentRepository.save(newComment);
+			} else {
+				CommentEntity newComment = CommentEntity.builder()
+					.id(commentId)
+					.content(comment.getContent())
+					.feed(comment.getFeed())
+					.isDeleted(IsDeletedType.N)
+					.user(user)
+					.build();
+
+				commentRepository.save(newComment);
+			}
+		} else {
+			throw new CustomException(ErrorCode.NO_ACCESS);
+		}
 
 	}
 
@@ -56,8 +124,18 @@ public class CommentService {
 	 *
 	 * @param commentId
 	 */
+	@Transactional
 	public void deleteComment(Long commentId) {
-
+		//피드 찾기
+		CommentEntity comment = commentRepository.findById(commentId).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+		//현재 사용자
+		Long userId = authService.getUserId();
+		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.ACCESS_TOKEN_NOT_FOUND));
+		if (user.equals(comment.getUser())) {
+			comment.setIsDeleted();
+		} else {
+			throw new CustomException(ErrorCode.NO_ACCESS);
+		}
 	}
 
 }
