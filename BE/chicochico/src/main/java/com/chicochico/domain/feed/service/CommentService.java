fb22afc2ed61db814e33a2091981dd4224d49b78
rlthdqls <rlthdqls@gmail.com>
2023-03-21 11:56:54 +0900

    [BE] feat : 댓글 멘션 리스트 조회

--- BE/chicochico/src/main/java/com/chicochico/domain/feed/service/CommentService.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/feed/service/CommentService.java
@@ -34,6 +34,8 @@ public class CommentService {
 
 	private AuthService authService;
 
+	private FollowEntity mentionUser;
+
 
 	/**
 	 * 해당 피드의 댓글 목록을 조회합니다.
@@ -51,19 +53,34 @@ public class CommentService {
 	}
 
 
+	/**
+	 * 내가 팔로우 한 사람 중 멘션할 유저 리스트 조회합니다.
+	 *
+	 * @param nickname
+	 * @return
+	 */
 	public List<FollowEntity> getMentionUserList(String nickname) {
 		//현재 사용자
 		Long userId = authService.getUserId();
 		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-		List<FollowEntity> mention = followRepository.findAllByFollowerAndFollowingNicknameStartingWith(user, nickname);
+		//사용자가 팔로우하는 유저 중 해당 글자로 시작하는 nickname
+		List<FollowEntity> mentionUserList = followRepository.findAllByFollowerAndFollowingNicknameStartingWith(user, nickname);
 
-		return mention;
+		return mentionUserList;
 	}
 
 
+	/**
+	 * mention할 유저 선택합니다.
+	 *
+	 * @param followId
+	 * @return
+	 */
 	public FollowEntity getMentionUser(Long followId) {
 		FollowEntity following = followRepository.findById(followId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+		//db에 저장할 때 사용할 것 (mentionId 추출)
+		mentionUser = following;
 		return following;
 	}
 
@@ -82,13 +99,14 @@ public class CommentService {
 		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 		//멘션 사용자 찾기
 		if (commentRequestDto.getMentionNickname() != null && !commentRequestDto.getMentionNickname().equals("")) {
-
+			//mention 할 사람 찾기
+			Long mentionId = mentionUser.getFollowing().getId();
 			//댓글 등록
-			//commentRepository.save(commentRequestDto.toEntity(feed, user, mentionId));
+			commentRepository.save(commentRequestDto.toEntity(feed, user, mentionId));
+		} else {
+			//멘션 사용자를 지정하지 않는 경우
+			commentRepository.save(commentRequestDto.toEntity(feed, user));
 		}
-		//멘션 없는 경우 댓글 등록
-		//		commentRepository.save(commentRequestDto.toEntity(feed, user));
-
 	}
 
 
@@ -99,8 +117,12 @@ public class CommentService {
 	 * @param commentRequestDto
 	 */
 	public void modifyComment(Long commentId, CommentRequestDto commentRequestDto) {
-		//피드 찾기
+
+		//댓글 찾기
 		CommentEntity comment = commentRepository.findById(commentId).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
+		//피드 찾기
+		FeedEntity feed = feedRepository.findById(comment.getFeed().getId()).orElseThrow(() -> new CustomException(ErrorCode.FEED_NOT_FOUND));
+
 		//현재 사용자
 		Long userId = authService.getUserId();
 		UserEntity user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
@@ -112,26 +134,14 @@ public class CommentService {
 				String nickname = commentRequestDto.getMentionNickname();
 				Long mentionId = userRepository.findByNickname(nickname).get().getId();
 				//댓글 등록
-				CommentEntity newComment = CommentEntity.builder()
-					.id(commentId)
-					.content(comment.getContent())
-					.feed(comment.getFeed())
-					.mentionUserId(mentionId)
-					.isDeleted(IsDeletedType.N)
-					.user(user)
-					.build();
-
+				CommentEntity newComment = commentRequestDto.toEntity(commentId, feed, user, mentionId);
 				commentRepository.save(newComment);
+
 			} else {
-				CommentEntity newComment = CommentEntity.builder()
-					.id(commentId)
-					.content(comment.getContent())
-					.feed(comment.getFeed())
-					.isDeleted(IsDeletedType.N)
-					.user(user)
-					.build();
-
+				//멘션 사용자가 없는 경우
+				CommentEntity newComment = commentRequestDto.toEntity(commentId, feed, user);
 				commentRepository.save(newComment);
+
 			}
 		} else {
 			throw new CustomException(ErrorCode.NO_ACCESS);
