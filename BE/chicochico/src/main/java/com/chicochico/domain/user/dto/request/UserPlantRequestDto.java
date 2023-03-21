37c30dec6ff4fe1d6da3142b88a2721e9dfddb78
sha990403 <sha990403@gmail.com>
2023-03-21 16:37:00 +0900

    [BE] feat: user plant Dto 내키식 구현 S08P22E101-275

--- BE/chicochico/src/main/java/com/chicochico/domain/user/dto/request/UserPlantRequestDto.java
+++ BE/chicochico/src/main/java/com/chicochico/domain/user/dto/request/UserPlantRequestDto.java
@@ -2,12 +2,20 @@ package com.chicochico.domain.user.dto.request;
 
 
 import com.chicochico.domain.user.entity.UserPlantEntity;
+import lombok.Builder;
+import lombok.Data;
 
 
 /**
  * 사용자가 키우는 식물 생성 요청
  */
+@Builder
+@Data
 public class UserPlantRequestDto {
+
+	private Long plantId;
+	private String plantNickname;
+
 
 	public UserPlantEntity toEntity() {
 		return (UserPlantEntity) new Object();
