7d68ab4e78c65d8dada467cac47ba7511a671bc7
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-03-21 16:14:55 +0900

    [FE] feat : 식물도감 리스트 이름 검색 구현
    S08P22E101-238 #done

--- FE/green-core/src/pages/api/plant/docs/index.ts
+++ FE/green-core/src/pages/api/plant/docs/index.ts
@@ -3,15 +3,67 @@ import { PlainResData } from '@/core/common/commonType';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<PlainResData>) {
   if (req.method === 'GET') {
-    res.status(200).json({
-      result: 'SUCCESS',
-      data: [
-        {
-          plantId: 1,
-          plantName: '가지',
+    const page = Number(req.query.page);
+
+    if (page % 2 === 0) {
+      res.status(200).json({
+        result: 'SUCCESS',
+        data: {
+          totalItemCount: 50,
+          plantList: [
+            {
+              plantId: 1,
+              plantName: '파',
+            },
+            {
+              plantId: 2,
+              plantName: '쪽파',
+            },
+            {
+              plantId: 3,
+              plantName: '대파',
+            },
+            {
+              plantId: 4,
+              plantName: '양파',
+            },
+            {
+              plantId: 5,
+              plantName: '어쩌구',
+            },
+          ],
         },
-      ],
-    });
+      });
+    } else {
+      res.status(200).json({
+        result: 'SUCCESS',
+        data: {
+          totalItemCount: 50,
+          plantList: [
+            {
+              plantId: 6,
+              plantName: '팽이버섯',
+            },
+            {
+              plantId: 7,
+              plantName: '양송이버섯',
+            },
+            {
+              plantId: 8,
+              plantName: '새송이버섯',
+            },
+            {
+              plantId: 9,
+              plantName: '느타리버섯',
+            },
+            {
+              plantId: 10,
+              plantName: '어쩌구',
+            },
+          ],
+        },
+      });
+    }
   } else {
     res.status(400).json({ result: 'FAIL', data: false });
   }
