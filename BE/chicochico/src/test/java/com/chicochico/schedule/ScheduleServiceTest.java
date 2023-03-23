874477698c65aed6efda2619fc2ad20a011107e1
rlthdqls <rlthdqls@gmail.com>
2023-03-23 11:04:03 +0900

    [BE] feat : Schedule Test Code

--- BE/chicochico/src/test/java/com/chicochico/schedule/ScheduleServiceTest.java
+++ BE/chicochico/src/test/java/com/chicochico/schedule/ScheduleServiceTest.java
@@ -5,7 +5,6 @@ import com.chicochico.common.code.IsCompletedType;
 import com.chicochico.common.code.IsDeletedType;
 import com.chicochico.common.code.ScheduleType;
 import com.chicochico.common.service.AuthService;
-import com.chicochico.domain.plant.entity.PlantEntity;
 import com.chicochico.domain.plant.repository.PlantRepository;
 import com.chicochico.domain.schedule.dto.request.ScheduleRequestDto;
 import com.chicochico.domain.schedule.entity.ScheduleEntity;
@@ -15,21 +14,21 @@ import com.chicochico.domain.user.entity.UserEntity;
 import com.chicochico.domain.user.entity.UserPlantEntity;
 import com.chicochico.domain.user.repository.UserPlantRepository;
 import com.chicochico.domain.user.repository.UserRepository;
-import com.chicochico.exception.CustomException;
-import com.chicochico.exception.ErrorCode;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 
@@ -50,81 +49,115 @@ public class ScheduleServiceTest {
 	@Mock
 	AuthService authService;
 
-	private UserEntity userEntity;
-	private PlantEntity plantEntity;
-	private UserPlantEntity userPlantEntity;
-	private ScheduleEntity scheduleEntity;
+	private MockMvc mockMvc;
 
 
 	@Test
 	public void 일정생성() {
 
+		UserEntity user = UserEntity.builder().id(1L).build();
 		when(authService.getUserId()).thenReturn(1L);
-		when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(UserEntity.builder().build()));
-		when(userPlantRepository.findById(any(Long.class))).thenReturn(Optional.of(UserPlantEntity.builder().build()));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		UserPlantEntity userPlant = UserPlantEntity.builder().id(1L).user(user).build();
+		when(userPlantRepository.findById(1L)).thenReturn(Optional.of(userPlant));
 		LocalDate localdate1 = LocalDate.of(2023, 4, 1);
-		LocalDate localdate2 = LocalDate.of(2023, 4, 3);
-		LocalDate localdate3 = LocalDate.of(2023, 4, 7);
-		LocalDate localdate4 = LocalDate.of(2023, 4, 30);
-		LocalDate localdate5 = LocalDate.of(2023, 5, 2);
 
 		ScheduleRequestDto schedule1 = ScheduleRequestDto.builder()
 			.content("content")
+			.userPlantId(1L)
 			.scheduleDate(localdate1)
 			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
-			.userPlantId(1L)
-			.build();
-		ScheduleRequestDto schedule2 = ScheduleRequestDto.builder()
-			.content("content")
-			.scheduleDate(localdate2)
-			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
-			.userPlantId(1L)
-			.build();
-		ScheduleRequestDto schedule3 = ScheduleRequestDto.builder()
-			.content("content")
-			.scheduleDate(localdate3)
-			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
-			.userPlantId(1L)
-			.build();
-		ScheduleRequestDto schedule4 = ScheduleRequestDto.builder()
-			.content("content")
-			.scheduleDate(localdate4)
-			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
-			.userPlantId(1L)
-			.build();
-		ScheduleRequestDto schedule5 = ScheduleRequestDto.builder()
-			.content("content")
-			.scheduleDate(localdate5)
-			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
-			.userPlantId(1L)
 			.build();
 
-		scheduleService.createSchedule(schedule1);
-		scheduleService.createSchedule(schedule2);
-		scheduleService.createSchedule(schedule3);
-		scheduleService.createSchedule(schedule4);
-		scheduleService.createSchedule(schedule5);
+		ScheduleEntity schedule = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate1)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
 
-		Assertions.assertThat(scheduleRepository.findAll().size() == 5);
+		assertThatCode(() -> {
+			scheduleService.createSchedule(schedule1);
+		}).doesNotThrowAnyException();
+		Mockito.verify(scheduleRepository, times(1)).save(refEq(schedule));
 	}
-	
+
 
 	@Test
 	@DisplayName("일정목록조회")
 	public void 일정목록조회() {
 
+		UserEntity user = UserEntity.builder().id(1L).build();
 		when(authService.getUserId()).thenReturn(1L);
-		when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(UserEntity.builder().build()));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		UserPlantEntity userPlant = UserPlantEntity.builder().id(1L).user(user).build();
 
 		LocalDate localdate1 = LocalDate.of(2023, 4, 1);
-		LocalDate localdate2 = LocalDate.of(2023, 4, 30);
-		LocalDate localdate3 = LocalDate.of(2023, 4, 4);
+		LocalDate localdate2 = LocalDate.of(2023, 4, 3);
+		LocalDate localdate3 = LocalDate.of(2023, 4, 7);
+		LocalDate localdate4 = LocalDate.of(2023, 4, 30);
+		LocalDate localdate5 = LocalDate.of(2023, 5, 2);
+		LocalDate localdate6 = LocalDate.of(2023, 5, 1);
+		LocalDate localdate7 = LocalDate.of(2023, 4, 4);
 
-		List<ScheduleEntity> result1 = scheduleService.getMonthSchedule(2023, 4);
-		Assertions.assertThat(result1.size() == 4);
+		ScheduleEntity schedule = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate1)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		scheduleRepository.save(schedule);
+		ScheduleEntity schedule1 = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate2)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		scheduleRepository.save(schedule1);
+		ScheduleEntity schedule2 = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate3)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		scheduleRepository.save(schedule2);
+		ScheduleEntity schedule3 = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate4)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		scheduleRepository.save(schedule3);
+		ScheduleEntity schedule4 = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate5)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		scheduleRepository.save(schedule4);
 
-		List<ScheduleEntity> result2 = scheduleService.getWeekSchedule(2023, 4, 1);
-		Assertions.assertThat(result2.size() == 2);
+		assertThatCode(() -> {
+			scheduleService.getMonthSchedule(2023, 4);
+		}).doesNotThrowAnyException();
+		Mockito.verify(scheduleRepository, times(1)).findAllByDateBetweenAndUser(localdate1, localdate6, user);
 	}
 
 
@@ -132,14 +165,28 @@ public class ScheduleServiceTest {
 	@DisplayName("일정삭제")
 	public void 일정삭제() {
 
+		UserEntity user = UserEntity.builder().id(1L).build();
 		when(authService.getUserId()).thenReturn(1L);
-		when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(UserEntity.builder().build()));
-		when(scheduleRepository.findById(any(Long.class))).thenReturn(Optional.of(ScheduleEntity.builder().build()));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		UserPlantEntity userPlant = UserPlantEntity.builder().id(1L).user(user).build();
 
-		ScheduleEntity schedule = scheduleRepository.findById(1L).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
-		scheduleService.deleteSchedule(1L);
+		LocalDate localdate1 = LocalDate.of(2023, 4, 1);
+		ScheduleEntity schedule = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate1)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		when(scheduleRepository.findById(1L)).thenReturn(Optional.of(schedule));
 
-		Assertions.assertThat(schedule.getIsDeleted() == IsDeletedType.Y);
+		assertThatCode(() -> {
+			scheduleService.deleteSchedule(1L);
+		}).doesNotThrowAnyException();
+		schedule.setIsDeleted(IsDeletedType.Y);
+		Mockito.verify(scheduleRepository, times(1)).save(refEq(schedule));
 
 	}
 
@@ -148,14 +195,28 @@ public class ScheduleServiceTest {
 	@DisplayName("일정완료")
 	public void 일정완료() {
 
+		UserEntity user = UserEntity.builder().id(1L).build();
 		when(authService.getUserId()).thenReturn(1L);
-		when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(UserEntity.builder().build()));
-		when(scheduleRepository.findById(any(Long.class))).thenReturn(Optional.of(ScheduleEntity.builder().build()));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		UserPlantEntity userPlant = UserPlantEntity.builder().id(1L).user(user).build();
+		LocalDate localdate1 = LocalDate.of(2023, 4, 1);
+		ScheduleEntity schedule = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate1)
+			.isCompleted(IsCompletedType.N)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		when(scheduleRepository.findById(1L)).thenReturn(Optional.of(schedule));
 
-		ScheduleEntity schedule = scheduleRepository.findById(1L).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
-		scheduleService.completeSchedule(1L);
+		schedule.setIsCompleted(IsCompletedType.Y);
 
-		Assertions.assertThat(schedule.getIsCompleted() == IsCompletedType.Y);
+		assertThatCode(() -> {
+			scheduleService.completeSchedule(1L);
+		}).doesNotThrowAnyException();
+		Mockito.verify(scheduleRepository, times(1)).save(refEq(schedule));
 
 	}
 
@@ -164,14 +225,28 @@ public class ScheduleServiceTest {
 	@DisplayName("일정완료취소")
 	public void 일정완료취소() {
 
+		UserEntity user = UserEntity.builder().id(1L).build();
 		when(authService.getUserId()).thenReturn(1L);
-		when(userRepository.findById(any(Long.class))).thenReturn(Optional.of(UserEntity.builder().build()));
-		when(scheduleRepository.findById(any(Long.class))).thenReturn(Optional.of(ScheduleEntity.builder().build()));
+		when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+		UserPlantEntity userPlant = UserPlantEntity.builder().id(1L).user(user).build();
+		LocalDate localdate1 = LocalDate.of(2023, 4, 1);
+		ScheduleEntity schedule = ScheduleEntity.builder()
+			.content("content")
+			.userPlant(userPlant)
+			.user(user)
+			.date(localdate1)
+			.isCompleted(IsCompletedType.Y)
+			.isDeleted(IsDeletedType.N)
+			.scheduleCode(ScheduleType.SCHEDULE_PRUNING)
+			.build();
+		when(scheduleRepository.findById(1L)).thenReturn(Optional.of(schedule));
 
-		ScheduleEntity schedule = scheduleRepository.findById(1L).orElseThrow(() -> new CustomException(ErrorCode.ENTITY_NOT_FOUND));
-		scheduleService.incompleteSchedule(1L);
+		schedule.setIsCompleted(IsCompletedType.N);
 
-		Assertions.assertThat(schedule.getIsCompleted() == IsCompletedType.N);
+		assertThatCode(() -> {
+			scheduleService.incompleteSchedule(1L);
+		}).doesNotThrowAnyException();
+		Mockito.verify(scheduleRepository, times(1)).save(refEq(schedule));
 
 	}
 
