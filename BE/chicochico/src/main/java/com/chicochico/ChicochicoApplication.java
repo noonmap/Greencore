6c2b8e23238a097c671a6ab89d1bb1b83c3e30a6
rlthdqls <rlthdqls@gmail.com>
2023-03-24 15:12:13 +0900

    [BE] feat : 스케쥴링 어노테이션 추가

--- BE/chicochico/src/main/java/com/chicochico/ChicochicoApplication.java
+++ BE/chicochico/src/main/java/com/chicochico/ChicochicoApplication.java
@@ -4,6 +4,7 @@ package com.chicochico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.Date;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaAuditing
 public class ChicochicoApplication {
 
