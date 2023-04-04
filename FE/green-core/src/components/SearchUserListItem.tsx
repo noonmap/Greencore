d21de237ab22324d10ae344432aa75e4b910ad14
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-04-05 00:02:54 +0900

    [FE] feat : 사용자검색 파이어베이스 프로필이미지 불러오기

--- FE/green-core/src/components/SearchUserListItem.tsx
+++ FE/green-core/src/components/SearchUserListItem.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { useRouter } from 'next/router';
 import { SearchUserType } from '../core/user/userType';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import styles from '@/components/SearchUserListItem.module.scss';
 
 export default function SearchUserListItem(props: { searchUser: SearchUserType }) {
   const searchUser = props.searchUser;
+  const storage = getStorage();
   const router = useRouter();
+
+  // 파이어베이스 이미지 불러오기
+  const [userProfileImagePath, setUserProfileImagePath] = useState<string>('');
+
+  useEffect(() => {
+    getUserProfile(searchUser.profileImagePath);
+  }, []);
+
+  function getUserProfile(nickname: string) {
+    const profileRef = ref(storage, `${nickname}/profileImage`);
+
+    getDownloadURL(profileRef)
+      .then((downloadURL) => {
+        setUserProfileImagePath(downloadURL);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   function goProfile() {
     router.push(`user/feed/${searchUser.nickname}`);
@@ -15,11 +36,7 @@ export default function SearchUserListItem(props: { searchUser: SearchUserType }
     <>
       <div key={searchUser.nickname} className={`${styles.container} p-5 flex items-center`} onClick={goProfile}>
         <div className='pr-5'>
-          {searchUser.profileImagePath ? (
-            <img src={searchUser.profileImagePath} width={50} height={50} alt='' />
-          ) : (
-            <Skeleton width={150} height={150} />
-          )}
+          {searchUser.profileImagePath ? <img src={userProfileImagePath} width={50} height={50} alt='' /> : <Skeleton width={150} height={150} />}
         </div>
         <div>{searchUser.nickname || <Skeleton />} </div>
       </div>
